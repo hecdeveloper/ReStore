dd8922b4cd41496eafac2bc45e13f879082929c9
Hector Ibarra <hector.dev.ui@gmail.com>
2023-10-09 22:27:36 -0600

    update react

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,23 @@
+const products = [
+  {name : 'Product 1', price: 100},
+  {name : 'Product 2', price: 200},
+]
+
 
 
 function App() {
-
   return (
     <>
-    <div>
+    <div className='app'>
       <h1>Re-Store</h1>
+      <ul>
+        {products.map((product) =>(
+          <li>
+            <h3>{product.name}</h3>
+            <p>{product.price}</p>
+          </li>
+        ))}
+      </ul>
     </div>
     </>
   )
