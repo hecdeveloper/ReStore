715a9518180254d2152182754d37cbcb52f2b237
Hector Ibarra <hector.dev.ui@gmail.com>
2023-10-11 16:12:01 -0600

    adding images

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,24 +1,26 @@
 import { useEffect, useState } from "react";
 import { Product } from "../models/products";
+import Catalog from "../../features/catalog/Catalog";
+import { Typography } from "@mui/material";
 
 
 
 function App() {
-  const [products, setProducts] = useState<Product[]>([ ]);
+  const [products, setProducts] = useState<Product[]>([]);
 
-  useEffect(() =>{
-   fetch('http://localhost:5000/api/products')
-    .then(response => response.json())
-    .then(data => setProducts(data))
-  },[])
+  useEffect(() => {
+    fetch('http://localhost:5000/api/products')
+      .then(response => response.json())
+      .then(data => setProducts(data))
+  }, [])
 
 
   function addProduct() {
-    setProducts(prevState => [...prevState, 
-      { 
+    setProducts(prevState => [...prevState,
+    {
       id: prevState.length + 101,
-      name: 'Product' + (prevState.length + 1), 
-      price: (prevState.length * 100) + 300.00, 
+      name: 'Product' + (prevState.length + 1),
+      price: (prevState.length * 100) + 300.00,
       brand: 'brand something',
       description: 'description something',
       pictureUrl: 'https://picsum.photos/200/300',
@@ -30,18 +32,12 @@ function App() {
   return (
     <>
       <div className='app'>
-        <h1>Re-Store</h1>
-        <ul>
-          {products.map((product) => (
-            <li key={product.id} >
-              {product.name}-{product.price}
-            </li>
-          ))}
-        </ul>
-        <button onClick={addProduct}>Add product</button>
+        <Typography variant="h1">Re-Store</Typography>
+        <Catalog products={products} addProduct={addProduct} />
       </div>
     </>
   )
 }
 
 export default App
+     
