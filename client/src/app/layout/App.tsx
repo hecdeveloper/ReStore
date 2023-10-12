9819e32ac3993e37256286f416748236fdd99289
Hector Ibarra <hector.dev.ui@gmail.com>
2023-10-12 12:32:01 -0600

    adding theme

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,44 +1,27 @@
-import { useEffect, useState } from "react";
+import { Container, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import Catalog from "../../features/catalog/Catalog";
-import { Product } from "../models/products";
 import Header from "./Header";
-import { Container, CssBaseline } from "@mui/material";
 
 
 
 function App() {
-  const [products, setProducts] = useState<Product[]>([]);
-
-  useEffect(() => { 
-    fetch('http://localhost:5000/api/products')
-      .then(response => response.json())
-      .then(data => setProducts(data))
-  }, [])
-
-
-  function addProduct() {
-    setProducts(prevState => [...prevState,
-    {
-      id: prevState.length + 101,
-      name: 'Product' + (prevState.length + 1),
-      price: (prevState.length * 100) + 300.00,
-      brand: 'brand something',
-      description: 'description something',
-      pictureUrl: 'https://picsum.photos/200/300',
-
-
-    }])
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
   }
-
+});
+ 
   return (
     <>
+    <ThemeProvider theme={theme}>
       <div className='app'>
         <CssBaseline />
         <Header/>
         <Container>
-        <Catalog products={products} addProduct={addProduct} />
+        <Catalog/>
         </Container>
       </div>
+      </ThemeProvider>
     </>
   )
 }
