b4a59a59eea647f0dec7a62ebd50be2d5711872d
Hector Ibarra <hector.dev.ui@gmail.com>
2023-10-18 12:22:09 -0600

    navbar styles

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,19 +1,34 @@
-import { AppBar, List, ListItem, Switch, Toolbar, Typography } from '@mui/material';
-import { NavLink } from 'react-router-dom';
+import { ShoppingCart } from "@mui/icons-material";
+import {
+    AppBar,
+    Badge,
+    IconButton,
+    List,
+    ListItem,
+    Switch,
+    Toolbar,
+    Typography,
+} from "@mui/material";
+import { NavLink } from "react-router-dom";
 
 const midLinks = [
-    {tittle: 'catalog', path: '/catalog'},
-    {tittle: 'about', path: '/about'},
-    {tittle: 'contact', path: '/contact'},
-]
+    { tittle: "catalog", path: "/catalog" },
+    { tittle: "about", path: "/about" },
+    { tittle: "contact", path: "/contact" },
+];
 
 const rightLinks = [
-    {tittle: 'login', path: '/login'},
-    {tittle: 'register', path: '/register'},
-]
+    { tittle: "login", path: "/login" },
+    { tittle: "register", path: "/register" },
+];
 
-
-
+const navStyles = {
+    color: "inherit",
+    textDecoration: "none",
+    typography: "h6",
+    "&:hover": { color: "secondary.main" },
+    "&.active": { color: "text.secondary" },
+};
 
 interface Props {
     darkMode: boolean;
@@ -22,37 +37,40 @@ interface Props {
 
 const Header = ({ darkMode, handleThemeChange }: Props) => {
     return (
-        <AppBar position='static' sx={{ mb: 4 }}>
-            <Toolbar> 
-                <Typography variant='h6'>Re-Store</Typography>
-                <Switch checked={darkMode} onChange={handleThemeChange}/>
-                <List sx={{display: 'flex'}}>
-                    {midLinks.map(({tittle, path}) => (
-                        <ListItem
-                        component={NavLink}
-                        to={path}
-                        key={path}
-                        sx={{color: 'inherit', textDecoration: 'none', mr: 4}}
-                        >
+        <AppBar position="static" sx={{ mb: 4 }}>
+            <Toolbar sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+
+                <Typography
+                    variant="h6"
+                    component={NavLink}
+                    to="/"
+                    sx={{ color: "inherit", textDecoration: "none" }}
+                >
+                    Re-Store
+                </Typography>
+                <Switch checked={darkMode} onChange={handleThemeChange} />
+                <List sx={{ display: "flex" }}>
+                    {midLinks.map(({ tittle, path }) => (
+                        <ListItem component={NavLink} to={path} key={path} sx={navStyles}>
                             {tittle.toUpperCase()}
                         </ListItem>
                     ))}
                 </List>
-                <List sx={{display: 'flex'}}>
-                    {rightLinks.map(({tittle, path}) => (
-                        <ListItem
-                        component={NavLink}
-                        to={path}
-                        key={path}
-                        sx={{color: 'inherit', textDecoration: 'none', mr: 4}}
-                        >
+                <IconButton size="large" edge="start" color="inherit" sx={{ mr: 2 }}>
+                    <Badge badgeContent="4" color="secondary">
+                        <ShoppingCart />
+                    </Badge>
+                </IconButton>
+                <List sx={{ display: "flex" }}>
+                    {rightLinks.map(({ tittle, path }) => (
+                        <ListItem component={NavLink} to={path} key={path} sx={navStyles}>
                             {tittle.toUpperCase()}
                         </ListItem>
                     ))}
                 </List>
             </Toolbar>
         </AppBar>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
